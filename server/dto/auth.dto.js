592b4394542403081118021671603ba0ef10ec0d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-12-06 09:37:03 +0530

    Update auth.dto.js

---
+++ server/dto/auth.dto.js
@@ -0,0 +1,88 @@
+// DTOs
+
+function RegistationDTO(username, email, password) {
+    if (!username || !email || !password) throw new Error("Missing Input Fields");
+    return { username: String(username), email: String(email).toLocaleLowerCase(), password: String(password) }
+}
+
+function loginDTO(email, password) {
+    if (!email || !password) throw new Error("Missing Input Fields");
+    return { email: String(email).toLocaleLowerCase(), password: String(password) }
+}
+
+function ForgetPasswordDTO(email) {
+    if(!email) throw new Error ("Missing Input Fields");
+    return { email: String(email) }
+}
+
+function VerifyEmailDTO(token, otp) {
+    if (!token || !otp) throw new Error("Missing Inputs");
+    return { token: String(token), otp: String(otp) }
+}
+
+function UpdatePasswordDTO(token, newpassword) {
+    if(!token || !newpassword) throw new Error("Missing Inputs");
+    return { token: String(token), newpassword: String(newpassword) }
+}
+
+
+// Response DTOs
+
+function RegistationResDTO(token, message = "Registation Successfull") {
+    return {
+        success: true,
+        token,
+        message
+    }
+}
+
+function EmailVerifyResDTO(message = "Email Verification Succcess") {
+    return {
+        success: true,
+        message
+    };
+}
+
+function LoginResDTO(token, user, message = "Login Success") {
+    return {
+        success: true,
+        token,
+        user: { id: user._id, email: user.email, username: user.username, role: user.role },
+        message
+    }
+}
+
+function ErrorResDTO(msg) {
+    return {
+        success: false, 
+        message: msg
+    };
+}
+
+function ForgetPasswordResDTO (token, message="OTP send to your email, Please check the Email") {
+    return {
+        success: true,
+        token,
+        message
+    }
+}
+
+function VerifyOTPResDTO (message="OTP Verify Success") {
+    return {
+        success: true,
+        message
+    }; 
+}
+
+function UpdatePasswordResDTO (message="Password Updated Successfully") {
+    return {
+        success: ture,
+        message,
+    }
+}
+
+module.exports = {
+    RegistationDTO, loginDTO, ForgetPasswordDTO, VerifyEmailDTO, UpdatePasswordDTO,
+    RegistationResDTO, LoginResDTO, ForgetPasswordResDTO, VerifyOTPResDTO, UpdatePasswordResDTO, EmailVerifyResDTO,
+    ErrorResDTO
+}
